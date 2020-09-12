614b4004a74ed4787840062dd169ea3144825ea6
Tofu <athenavladimir@gmail.com>
2020-09-12 18:45:45 +0700

    TravellingApp

---
+++ TravellingAppSwiftUI/TravellingAppSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TravellingAppSwiftUI
+//
+//  Created by Apple on 9/12/20.
+//  Copyright © 2020 Tofu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
